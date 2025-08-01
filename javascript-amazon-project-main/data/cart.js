e27f1b6b26d8e4a17879ff0ad0002aebbf0fa250
Gulrukhsor Akhmadjanova <akhmadjanovagulrukhsor@gmail.com>
2025-08-01 14:48:29 +0500

    test-framework

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,18 +1,26 @@
-export let cart= JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+   cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
 cart = [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '1'
   },
   {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }
 ]
 }
 
+}
 
 
 function saveToStorage() {
@@ -31,7 +39,8 @@ export function addToCart(productId) {
       } else { 
         cart.push({
           productId,
-          quantity: 1
+          quantity: 1,
+          deliveryOptionId: '1'
       });
       }
 
@@ -49,3 +58,15 @@ export function removeFromCart(productId){
   cart = newCart;
   saveToStorage();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId){
+      matchingItem =cartItem;
+    }
+  });
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveToStorage();
+}
+
